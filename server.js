dec2b7f284e6fb15a5fec5689bc069629d69091e
Akriti Bhusal <akritibhusal@gmail.com>
2023-09-10 00:17:42 +1000

    Add database

--- server.js
+++ server.js
@@ -1,44 +1,84 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
-const dotenv = require("dotenv").config();
+const { createClient } = require("@supabase/supabase-js");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
-let jokesData = [];
+const options = {
+  auth: {
+    persistSession: true,
+    storageKey: "supabase",
+  },
+};
 
-app.use(express.static('public'));
+const supabase = createClient(
+  process.env.DB_HOST,
+  process.env.API_KEY,
+  options
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
 app.use(cors());
 
-const getJokes = (res) => {
-  try {
-    jokesData = JSON.parse(fs.readFileSync("./jokes.json", "utf8"));
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({status: 500, message: "Error reading jokes file"});
-  }
-}
-
-// Routes
+/**
+ * Routes
+ */
 app.get("/status", (req, res) => {
-  getJokes(res);
-  res.send({status: 200, message: "We are live and joking!"});
+  res.send({ status: 200, message: "We are live and joking!" });
 });
 
-app.get("/jokes", (req, res) => {
-  getJokes(res);
-  res.send(jokesData);
+app.get("/jokes", async (req, res) => {
+  const { data, error } = await supabase.from("jokes").select();
+
+  if (error) {
+    res.status(500).send({ status: 500, message: error });
+  }
+
+  res.send(data);
 });
 
-app.get("/jokes/:id", (req, res) => {
-  getJokes(res);
-  const requestedJoke = jokesData.find((joke) => joke.id === parseInt(req.params.id))
-  if (requestedJoke) res.send(requestedJoke);
-  res.status(404).send({status: 404, message: "Joke not found"});
+app.get("/jokes/:id", async (req, res) => {
+  const { data, error } = await supabase
+    .from("jokes")
+    .select()
+    .eq("id", req.params.id);
+
+  if (error) {
+    res.status(500).send({ status: 500, message: error });
+  }
+  if (!data.length) {
+    res.status(404).send({ status: 404, message: "Joke not found" });
+  }
+  res.send(data);
+});
+
+app.post("/joke", async (req, res) => {
+  const { error } = await supabase
+    .from("jokes")
+    .insert({ setup: req.body.setup, punchline: req.body.punchline });
+  if (error) {
+    res.status(500).send({ status: 500, message: error });
+  }
+  res.send({ status: 200, message: "Joke added" });
+});
+
+app.post("/jokes", async (req, res) => {
+  // TODO: Validate the request body using zod or joi
+  const jokesList = req.body.map((joke) => {
+    return { setup: joke.setup, punchline: joke.punchline };
+  });
+  const { error } = await supabase.from("jokes").insert(jokesList);
+  if (error) {
+    res.status(500).send({ status: 500, message: error });
+  }
+  res.send({ status: 200, message: "Jokes added 👏" });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at port :${port}, JK!`);
 });
-
